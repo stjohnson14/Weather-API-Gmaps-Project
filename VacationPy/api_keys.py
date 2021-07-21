34047068fcc96cdf26a7cc0d071778e6b779a662
Stefan Johnson <stefan.johnson14@gmail.com>
2021-07-21 19:49:48 -0400

    finished weatherpy

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "01cee722765ab785ef3c09df28fd5845"
+
+# Google API Key
+g_key = "AIzaSyCwuc1Ab0j-x8tyuE0Yvamr3ctPkb-rcnw"
---
+++ WeatherPy/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,1 @@
+api_key = "01cee722765ab785ef3c09df28fd5845"
